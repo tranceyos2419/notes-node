ec21515a19f988cb9fd8af9bfc24bd7cf767a656
tranceyos2419 <tranceyos2419@gmail.com>
2019-01-30 10:53:49 +0900

    S3-L13: Getting Input From User

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-const _ = require('lodash');
+var command = process.argv;
 
-var filterdArray = _.uniq([2, 3, 2, 1])
-
-console.log(filterdArray);
+console.log(command[2]);
+if (command[2] === 'read') {
+    console.log('reading it');
+} else if (command[2] === 'list') {
+    console.log('listing it');
+} else if (command[2] === 'note') {
+    console.log('noting it');
+} else {
+    console.log('command is not recognized');
+}
