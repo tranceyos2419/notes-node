40d2290dd9f90137899b5610c8469fc44aa03e69
tranceyos2419 <tranceyos2419@gmail.com>
2019-02-10 19:49:47 +0900

    L43

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Home page",
+    currentYear: new Date().getFullYear(),
+    welcome: "Welcome to my page"
+  });
+});
+
+app.get("/hello", (req, res) => {
   res.send("<h1>Hello Express</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About page",
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get("/json", (req, res) => {
