ce7c7d2f51a3a26b6210db1981e4af265727ce73
tranceyos2419 <tranceyos2419@gmail.com>
2019-02-12 19:14:24 +0900

    L45

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 //! type "nodemon app.js -e js,hbs" to watch changes
 const express = require("express");
 const hbs = require("hbs");
+const fs = require("fs");
+
 const app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) console.log("Unable to append to sever.log");
+  });
+  next();
+});
+
+app.use((req, res) => {
+  res.render("maintenance.hbs");
+});
+
 app.use(express.static(__dirname + "/public"));
 
 hbs.registerHelper("getCurrentYear", () => {
