113bfad485623188392a74f0b9ee87fe2457d90a
tranceyos2419 <tranceyos2419@gmail.com>
2019-02-16 18:53:52 +0900

    L57

--- server/server.test.js
+++ server/server.test.js
@@ -2,26 +2,31 @@ const request = require("supertest");
 
 let app = require("./server").app;
 
-it("should return hello as the response", done => {
-  request(app)
-    .get("/")
-    .expect(404)
-    // .expect(res => {
-    //   expect(res.body).toInclude({
-    //     error: "Page not found"
-    //   });
-    // })
-    .end(done);
-});
-
-it("should have an object which has Yoshi as the name property", done => {
-  request(app)
-    .get("/user")
-    .expect(200)
-    // .expect(res => {
-    //   expect(res.body).toInclude({
-    //     name: "Yoshi"
-    //   });
-    // })
-    .end(done);
+describe("server test", () => {
+  describe("GET /", () => {
+    it("should return hello as the response", done => {
+      request(app)
+        .get("/")
+        .expect(404)
+        // .expect(res => {
+        //   expect(res.body).toInclude({
+        //     error: "Page not found"
+        //   });
+        // })
+        .end(done);
+    });
+  });
+  describe("GET /user", () => {
+    it("should have an object which has Yoshi as the name property", done => {
+      request(app)
+        .get("/user")
+        .expect(200)
+        // .expect(res => {
+        //   expect(res.body).toInclude({
+        //     name: "Yoshi"
+        //   });
+        // })
+        .end(done);
+    });
+  });
 });
--- utils/utilsAsync.test.js
+++ utils/utilsAsync.test.js
@@ -1,20 +1,22 @@
 const utils = require("./utils");
 const expect = require("expect");
 
-it("should async add two numbers", done => {
-  utils.asyncAdd(4, 3, sum => {
-    expect(sum)
-      .toBe(7)
-      .toBeA("number");
-    done();
+describe("utils with async", () => {
+  it("should async add two numbers", done => {
+    utils.asyncAdd(4, 3, sum => {
+      expect(sum)
+        .toBe(7)
+        .toBeA("number");
+      done();
+    });
   });
-});
 
-it("should async square two numbers", done => {
-  utils.asyncSquare(4, multi => {
-    expect(multi)
-      .toBe(16)
-      .toBeA("number");
-    done();
+  it("should async square two numbers", done => {
+    utils.asyncSquare(4, multi => {
+      expect(multi)
+        .toBe(16)
+        .toBeA("number");
+      done();
+    });
   });
 });
