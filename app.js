59caef31120fa2e61efa1bf71f4ea34c1adf7569
tranceyos2419 <tranceyos2419@gmail.com>
2019-02-27 11:30:26 +0900

    new setup

--- app.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const os = require('os');
-const notes = require('./notes')
-
-// fs.appendFileSync('hello.txt', `Hello ${os.userInfo().username}`);
-// console.log(notes.age);
-
-console.log(notes.addNote());
--- notes.js
+++
@@ -1,7 +0,0 @@
-// console.log(module);
-// module.exports.age = 25;
-
-module.exports.addNote = () => {
-    console.log('Notes');
-    // return 'Add notes'
-}
