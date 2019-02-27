e622e5b7ce9380cd951fc3bbb4e2ed0fddc7ef95
tranceyos2419 <tranceyos2419@gmail.com>
2019-02-27 12:09:47 +0900

    see other commits

---
+++ server/models/todo.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+module.exports = Todo = mongoose.model("todos", TodoSchema);
